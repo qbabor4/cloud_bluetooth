05af612cf32a166361d4d67162c132e7664199c6
jakub <qbabor4444@gmail.com>
2017-07-03 15:40:11 +0200

    bluetooth app start

---
+++ bluetooth.ino
@@ -0,0 +1,28 @@
+/*
+* 
+*/
+
+//TODO
+// dostac caly hex string i dopiero wtedy coś robic
+String recivedData;            //Variable for storing received data
+void setup()
+{
+    Serial.begin(9600);   //Sets the baud for serial data transmission                               
+    pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+}
+void loop()
+{
+      while ( Serial.available() > 0 ) {     // Get data only when you receive data:
+          char c = Serial.read();        //Read the incoming data & store into c
+          recivedData += c;
+          //Serial.print( recivedData );          
+          Serial.print (Serial.available()); 
+          //Serial.print("\n");
+          Serial.print("\n");     
+          if( recivedData == '1')              // Checks whether value of data is equal to 1
+             digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
+          else if( recivedData == '0')         //  Checks whether value of data is equal to 0
+             digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
+   }
+  }
+}
