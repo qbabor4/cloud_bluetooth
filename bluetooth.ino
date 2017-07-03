2478464411004f6d32333dbbf5888c23f77dae5e
jakub <qbabor4444@gmail.com>
2017-07-03 19:34:16 +0200

    gets ehole string

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,14 +1,31 @@
 /*
  * Bluetooth controled program, to comunicate cloud with Android app
- * Reciving data via bluetooth with end-marker '>'
+ * Reciving data via bluetooth with end-marker '>' like a2b1b4>
+ *
+ * 
 */
 
 //TODO
-// dostac caly hex string i dopiero wtedy coś robic
+// dostac caly hex string i dopiero wtedy coś robic (end marker)
+// zobaczyc czy zamiana z hex na dec zajmuje duzo wiecej czasu od wysyłąnia w dec
 
-String recivedData;            //Variable for storing received data
-const byte numChars = 32;
-char receivedChars[numChars]; // array of data
+#include <PololuLedStrip.h>
+// Create an ledStrip object and specify the pin it will use.
+PololuLedStrip<6> ledStrip;
+
+const int LED_COUNT = 21;
+rgb_color colors[LED_COUNT];
+//rgb_color color;
+
+//color variables
+byte red, green, blue;
+
+// Create a buffer for holding the colors (3 bytes per color). example: [255,255,255]
+#define LED_COUNT 21 // numbers of leds + 1
+
+String receivedData = "";
+//const byte numChars = 32;
+//char receivedData[numChars]; // array of data
 
 void setup()
 {
@@ -19,14 +36,15 @@ void loop()
 {
       if ( Serial.available() > 0 ) {     // Get data only when you receive data:
           char c = Serial.read();        //Read the incoming data & store into c
-          recivedData += c;
-          Serial.print( recivedData );          
-          Serial.print("\n");
-          Serial.print("\n");     
-          if( recivedData == '1')              // Checks whether value of data is equal to 1
-             digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
-          else if( recivedData == '0')         //  Checks whether value of data is equal to 0
-             digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
+          receivedData += c;
+           
+          if ( c == '>' ){
+            Serial.print( receivedData );          
+            Serial.print("\n");
+            Serial.print("\n");
+            digitalWrite(13, HIGH); 
+            receivedData = "";
+          }
    }
-  }
+  
 }
